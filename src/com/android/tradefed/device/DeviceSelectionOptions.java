24745655a4069b9f3699451d4274bd1bb4a4f735
Tsu Chiang Chuang <tsu@google.com>
2014-09-29 22:03:49 +0000

    Revert "Set min battery level default to 10%" Reverting this comment as per xsdg@ comment on affecting external clients of TF.
    This reverts commit d8b6ceb08c8b9aa1b893781972ac09be7acbe822.
    
    Change-Id: I9fea9e6f96a6c63a04ec284a9281cfd7d1f593ac

--- src/com/android/tradefed/device/DeviceSelectionOptions.java
+++ src/com/android/tradefed/device/DeviceSelectionOptions.java
@@ -71,7 +71,7 @@ public class DeviceSelectionOptions implements IDeviceSelection {
     @Option(name = "min-battery", description =
         "only run this test on a device whose battery level is at least the given amount. " +
         "Scale: 0-100")
-    private Integer mMinBattery = 10;
+    private Integer mMinBattery = null;
 
     @Option(name = "max-battery", description =
         "only run this test on a device whose battery level is strictly less than the given " +
