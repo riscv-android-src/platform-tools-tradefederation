bc79e7ac5d748890a94eab807d46a8ad47d12d6a
Guang Zhu <guangzhu@google.com>
2015-04-27 18:42:19 -0700

    fix dm verity detection in remount
    also added some test cases and refactoring of TestDeviceTest
    
    Change-Id: Idab2d7c39599cbb81ea376d52ed1b59e521430f1

--- src/com/android/tradefed/device/TestDevice.java
+++ src/com/android/tradefed/device/TestDevice.java
@@ -3255,8 +3255,18 @@ class TestDevice implements IManagedTestDevice {
      */
     @Override
     public void remountSystemWritable() throws DeviceNotAvailableException {
-        String systemVerifiedState = getProperty("partition.system.verified");
-        if ("1".equals(systemVerifiedState)) {
+        String verity = getProperty("partition.system.verified");
+        long verityVersion = 0;
+        if (verity != null && !verity.isEmpty()) {
+            try {
+                verityVersion = Long.parseLong(verity);
+            } catch (NumberFormatException nfe) {
+                // ignore but assign an arbitrary number since it's non-empty
+                CLog.d("unrecognized property value partition.system.verified=%s", verity);
+                verityVersion = 1;
+            }
+        }
+        if (verityVersion > 0) {
             executeAdbCommand("disable-verity");
             reboot();
         }
