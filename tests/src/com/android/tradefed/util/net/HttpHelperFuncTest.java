4f8a1cfe3898b5a761ae082c228c1ad6c6ef1c89
Julien Desprez <jdesprez@google.com>
2016-04-08 11:39:17 +0100

    Fix a some of the TF func test consistently failing
    Bug: 27996907
    Change-Id: I6143f3a81d0512e813d17eb12e3e47a95e757780

--- tests/src/com/android/tradefed/util/net/HttpHelperFuncTest.java
+++ tests/src/com/android/tradefed/util/net/HttpHelperFuncTest.java
@@ -113,8 +113,9 @@ public class HttpHelperFuncTest extends TestCase {
         public ServerSocket mSS = null;
         public Throwable mException = null;
         public static final String RESPONSE = "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: text/plain\r\n\r\n" +
-                "Hello, this is dog.\r\n";
+                "Content-Length: 30\r\n" +
+                "Content-Type: text/html\r\n\r\n" +
+                "<h1>Hello, this is dog.</h1>\r\n";
 
         private final int mDelay;
         private final int mTimeout;
