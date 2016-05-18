f3e72a76a0b9f513622f69dcd33f3dac0ca06b61
Julien Desprez <jdesprez@google.com>
2016-05-18 11:30:30 +0100

    Fix FileUtil fallback for chmod.
    Differentiate chmod failures from chmod not available due
    to the system OS.
    
    Bug: 28642204
    Change-Id: I04aaaa0a73e6355374aedd5dd7cd2f5393537a44

--- tests/src/com/android/tradefed/util/FileUtilTest.java
+++ tests/src/com/android/tradefed/util/FileUtilTest.java
@@ -15,15 +15,106 @@
  */
 package com.android.tradefed.util;
 
+import com.android.tradefed.log.LogUtil.CLog;
+
 import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Unit tests for {@link FileUtil}
  */
 public class FileUtilTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        FileUtil.setChmodBinary("chmod");
+    }
+
+    /**
+     * test {@link FileUtil#getExtension(String)}
+     */
     public void testGetExtension() {
         assertEquals("", FileUtil.getExtension("filewithoutext"));
         assertEquals(".txt", FileUtil.getExtension("file.txt"));
         assertEquals(".txt", FileUtil.getExtension("foo.file.txt"));
+    }
+
+    /**
+     * test {@link FileUtil#chmodGroupRW(File)} on a system that supports 'chmod'
+     */
+    public void testChmodGroupRW() throws IOException {
+        File testFile = null;
+        try {
+            if (!FileUtil.chmodExists()) {
+                CLog.d("Chmod not available, skipping the test");
+                return;
+            }
+            testFile = FileUtil.createTempFile("testChmodRW", ".txt");
+            assertTrue(FileUtil.chmodGroupRW(testFile));
+            assertTrue(testFile.canRead());
+            assertTrue(testFile.canWrite());
+            assertFalse(testFile.canExecute());
+        } finally {
+            FileUtil.deleteFile(testFile);
+        }
+    }
+
+    /**
+     * test {@link FileUtil#chmodGroupRW(File)} on a system that does not supports 'chmod'.
+     * File permission should still be set with the fallback.
+     */
+    public void testChmodGroupRW_noChmod() throws IOException {
+        File testFile = null;
+        FileUtil.setChmodBinary("fake_not_existing_chmod");
+        try {
+            testFile = FileUtil.createTempFile("testChmodRW", ".txt");
+            assertTrue(FileUtil.chmodGroupRW(testFile));
+            assertTrue(testFile.canRead());
+            assertTrue(testFile.canWrite());
+            assertFalse(testFile.canExecute());
+        } finally {
+            FileUtil.deleteFile(testFile);
+        }
+    }
+
+    /**
+     * test {@link FileUtil#chmodGroupRWX(File)} on a system that supports 'chmod'
+     */
+    public void testChmodGroupRWX() throws IOException {
+        File testFile = null;
+        try {
+            if (!FileUtil.chmodExists()) {
+                CLog.d("Chmod not available, skipping the test");
+                return;
+            }
+            testFile = FileUtil.createTempFile("testChmodRWX", ".txt");
+            assertTrue(FileUtil.chmodGroupRWX(testFile));
+            assertTrue(testFile.canRead());
+            assertTrue(testFile.canWrite());
+            assertTrue(testFile.canExecute());
+        } finally {
+            FileUtil.deleteFile(testFile);
+        }
+    }
+
+    /**
+     * test {@link FileUtil#chmodGroupRWX(File)} on a system that does not supports 'chmod'.
+     * File permission should still be set with the fallback.
+     */
+    public void testChmodGroupRWX_noChmod() throws IOException {
+        File testFile = null;
+        FileUtil.setChmodBinary("fake_not_existing_chmod");
+        try {
+            testFile = FileUtil.createTempFile("testChmodRWX", ".txt");
+            assertTrue(FileUtil.chmodGroupRWX(testFile));
+            assertTrue(testFile.canRead());
+            assertTrue(testFile.canWrite());
+            assertTrue(testFile.canExecute());
+        } finally {
+            FileUtil.deleteFile(testFile);
+        }
     }
 }
