a8cd836c35b23e7bede575d5ef0dc62980e968c9
Xing Dai <xingdai@google.com>
2017-07-13 18:06:54 +0000

    Revert "Skip post flash flavor check."
    This reverts commit 68fe0051eb24b74736650174a975c9080a49c418.
    
    Change-Id: I4193ccf836105dea2fd8252ac6fc03c30c6d2b5c

--- tests/src/com/android/tradefed/targetprep/DeviceFlashPreparerTest.java
+++ tests/src/com/android/tradefed/targetprep/DeviceFlashPreparerTest.java
@@ -117,6 +117,7 @@ public class DeviceFlashPreparerTest {
         EasyMock.expect(mMockDevice.enableAdbRoot()).andStubReturn(Boolean.TRUE);
         mMockDevice.setDate(null);
         EasyMock.expect(mMockDevice.getBuildId()).andReturn(mMockBuildInfo.getBuildId());
+        EasyMock.expect(mMockDevice.getBuildFlavor()).andReturn(mMockBuildInfo.getBuildFlavor());
         EasyMock.expect(mMockDevice.isEncryptionSupported()).andStubReturn(Boolean.TRUE);
         EasyMock.expect(mMockDevice.isDeviceEncrypted()).andStubReturn(Boolean.FALSE);
         mMockDevice.clearLogcat();
@@ -153,6 +154,7 @@ public class DeviceFlashPreparerTest {
         EasyMock.expect(mMockDevice.enableAdbRoot()).andStubReturn(Boolean.TRUE);
         mMockDevice.setDate(null);
         EasyMock.expect(mMockDevice.getBuildId()).andReturn(mMockBuildInfo.getBuildId());
+        EasyMock.expect(mMockDevice.getBuildFlavor()).andReturn(mMockBuildInfo.getBuildFlavor());
         EasyMock.expect(mMockDevice.isEncryptionSupported()).andStubReturn(Boolean.TRUE);
         EasyMock.expect(mMockDevice.isDeviceEncrypted()).andStubReturn(Boolean.FALSE);
         EasyMock.expect(mMockDevice.getProductType()).andReturn("flavor");
