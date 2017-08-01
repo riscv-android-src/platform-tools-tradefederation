55caf460efd6a2cccb54629a12d3cdac96fa0d36
Arthur Eubanks <aeubanks@google.com>
2017-08-01 15:05:13 -0700

    Add InstallAllTestZipAppsSetup
    InstallAllTestZipAppsSetup takes a zip file name, unzips it, and
    installs all APKs it finds in it.
    
    Test: run_tradefed_tests.sh
    
    Change-Id: I188629a3af83f1d8ce3c146a16054ba703444ec1

--- tests/src/com/android/tradefed/UnitTests.java
+++ tests/src/com/android/tradefed/UnitTests.java
@@ -105,6 +105,7 @@ import com.android.tradefed.targetprep.DeviceFlashPreparerTest;
 import com.android.tradefed.targetprep.DeviceSetupTest;
 import com.android.tradefed.targetprep.FastbootDeviceFlasherTest;
 import com.android.tradefed.targetprep.FlashingResourcesParserTest;
+import com.android.tradefed.targetprep.InstallAllTestZipAppsSetupTest;
 import com.android.tradefed.targetprep.InstallApkSetupTest;
 import com.android.tradefed.targetprep.InstrumentationPreparerTest;
 import com.android.tradefed.targetprep.KernelFlashPreparerTest;
@@ -336,6 +337,7 @@ import org.junit.runners.Suite.SuiteClasses;
     DeviceSetupTest.class,
     FastbootDeviceFlasherTest.class,
     FlashingResourcesParserTest.class,
+    InstallAllTestZipAppsSetupTest.class,
     InstallApkSetupTest.class,
     InstrumentationPreparerTest.class,
     KernelFlashPreparerTest.class,
