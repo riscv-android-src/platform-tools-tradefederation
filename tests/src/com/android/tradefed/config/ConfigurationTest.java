5a589cbb23c115f34fe439f43117627eef50a211
jdesprez <jdesprez@google.com>
2017-08-03 16:58:11 -0700

    Increase hprof depth to see true root cause in deep stack
    Some stack are very deep and require more depth in the
    look up to find their origin.
    
    Test: unit tests
    Change-Id: I809d6679a1d2af509f8f1fa4492af01ccd6eeeaf

--- tests/src/com/android/tradefed/config/ConfigurationTest.java
+++ tests/src/com/android/tradefed/config/ConfigurationTest.java
@@ -26,6 +26,7 @@ import com.android.tradefed.device.IDeviceRecovery;
 import com.android.tradefed.device.IDeviceSelection;
 import com.android.tradefed.invoker.InvocationContext;
 import com.android.tradefed.log.ILeveledLogOutput;
+import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.result.ITestInvocationListener;
 import com.android.tradefed.result.TextResultReporter;
 import com.android.tradefed.targetprep.ITargetPreparer;
@@ -637,6 +638,7 @@ public class ConfigurationTest extends TestCase {
             String content = FileUtil.readStringFromFile(test);
             assertTrue(content.length() > 100);
             assertTrue(content.contains("<configuration>"));
+            CLog.e("%s", content);
         } finally {
             FileUtil.deleteFile(test);
         }
