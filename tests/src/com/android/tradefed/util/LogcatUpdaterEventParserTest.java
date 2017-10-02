0a2dd6ef2edb586faf1ff98b4c474442e73001d7
GuangHui Liu <ghliu@google.com>
2017-10-02 16:18:19 -0700

    Clean up logcatUpdaterEventParser, enforce wait timeout.
    - waitForEvent and waitForEventAsync must have timeout
    - add additional triggers for events and errors
    - refactor and simplify UpdaterEventTrigger
    - add unit test cases
    
    Bug: 66996067
    Bug: 67419887
    Test: new & existing unit tests
    Test: forrest
    Change-Id: I561bd4aff4df4ff6280d8a1fc12f17e97a619784

--- tests/src/com/android/tradefed/util/LogcatUpdaterEventParserTest.java
+++ tests/src/com/android/tradefed/util/LogcatUpdaterEventParserTest.java
@@ -18,6 +18,7 @@ package com.android.tradefed.util;
 
 import com.android.tradefed.device.ILogcatReceiver;
 import com.android.tradefed.result.InputStreamSource;
+import com.android.tradefed.util.LogcatUpdaterEventParser.AsyncUpdaterEvent;
 
 import junit.framework.TestCase;
 
@@ -33,7 +34,22 @@ import java.io.PipedOutputStream;
  */
 public class LogcatUpdaterEventParserTest extends TestCase {
 
-    private static final long SHORT_WAIT_MS = 100L;
+    private static final String[] LOGS_UPDATE_COMPLETE = {
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: Update successfully applied\n",
+    };
+    private static final String[] LOGS_ERROR = {
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
+        "11-11 00:00:00.001  123 321 I update_engine: "
+                + "ActionProcessor: Aborting processing due to failure.\n",
+    };
+    private static final long SMALL_WAIT_TIME_MS = 1000L;
 
     private ILogcatReceiver mMockReceiver = null;
     private LogcatUpdaterEventParser mParser = null;
@@ -74,87 +90,154 @@ public class LogcatUpdaterEventParserTest extends TestCase {
     }
 
     /**
-     * Test that a known event parses to the expected {@link UpdaterEventType} when a portion
-     * of the trigger contains the matched line.
+     * Test that a known event parses to the expected {@link UpdaterEventType} when there is an
+     * exact match.
      */
-    public void testParseEventType_mappedEventPartialLine() {
-        String mappedLogLine = "11-11 00:00:00.001  123 321 I update_engine:" +
-                " Update successfully applied";
-        assertEquals(UpdaterEventType.UPDATE_COMPLETE, mParser.parseEventType(mappedLogLine));
-    }
-
-    /**
-     * Test that a known event parses to the expected {@link UpdaterEventType} when the
-     * expected trigger contains only a portion of the matched line.
-     */
-    public void testParseEventType_mappedEventPartialTrigger() {
-        String mappedLogLine = "11-11 00:00:00.001  123 321 I dex2oat: dex2oat took 12345 seconds";
-        assertEquals(mParser.parseEventType(mappedLogLine), UpdaterEventType.D2O_COMPLETE);
-    }
-
-    /**
-     * Test that a known event parses to the expected {@link UpdaterEventType} when there is
-     * an exact match.
-     */
-    public void testParseEventType_mappedEventFull() {
+    public void testParseEventTypeExactMatch() {
         String mappedLogLine =
                 "11-11 00:00:00.001  123 321 I update_verifier: Leaving update_verifier.";
-        assertEquals(mParser.parseEventType(mappedLogLine),
-                UpdaterEventType.UPDATE_VERIFIER_COMPLETE);
+        assertEquals(
+                UpdaterEventType.UPDATE_VERIFIER_COMPLETE, mParser.parseEventType(mappedLogLine));
     }
 
     /**
-     * Test that unknown events parse to null.
+     * Test that a known event parses to the expected {@link UpdaterEventType} when the registered
+     * message is a substring of the log message.
      */
-    public void testParseEventType_unmappedEvent() {
+    public void testParseEventTypePartialMatch() {
+        String log =
+                "01-09 17:06:50.799  8688  8688 I update_engine_client: "
+                        + "onPayloadApplicationComplete(ErrorCode::kUserCanceled (48))";
+        assertEquals(UpdaterEventType.ERROR, mParser.parseEventType(log));
+    }
+
+    /** Test that unknown events parse to null. */
+    public void testParseEventTypeUnknown() {
         String unmappedLogLine = "11-11 00:00:00.001  123 321 I update_engine: foo bar baz";
-        assertEquals(mParser.parseEventType(unmappedLogLine), null);
+        assertEquals(null, mParser.parseEventType(unmappedLogLine));
         unmappedLogLine = "11-11 00:00:00.001  123 321 I foobar_engine: Update succeeded";
-        assertEquals(mParser.parseEventType(unmappedLogLine), null);
+        assertEquals(null, mParser.parseEventType(unmappedLogLine));
         unmappedLogLine = "11-11 00:00:00.001  123 321 I foobar_engine: foo bar baz";
-        assertEquals(mParser.parseEventType(unmappedLogLine), null);
+        assertEquals(null, mParser.parseEventType(unmappedLogLine));
     }
 
-    /** Test that a thread exits its wait loop when it sees any event. */
-    public void testWaitForEvent_any() throws Exception {
-        Thread waitThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mParser.waitForEvent();
-            }
-        });
-        waitThread.setName(getClass().getCanonicalName());
-        String[] logLines = {
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: Update successfully applied\n"
-            };
-        waitAndAssertTerminated(waitThread, logLines);
+    /** Test that parser recognize update engine error message */
+    public void testParseEventTypeError() {
+        String error1 =
+                "01-09 17:06:50.799  8688  8688 I update_engine_client: "
+                        + "onPayloadApplicationComplete(ErrorCode::kUserCanceled (48))";
+        assertEquals(UpdaterEventType.ERROR, mParser.parseEventType(error1));
+        String error2 =
+                "04-05 10:56:20.026   172   172 I update_engine: "
+                        + "ActionProcessor: Aborting processing due to failure.";
+        assertEquals(UpdaterEventType.ERROR, mParser.parseEventType(error2));
     }
 
-    /** Test that a thread exits its wait loop when it sees a specific expected event. */
-    public void testWaitForEvent_specific() throws Exception {
-        Thread waitThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mParser.waitForEvent(UpdaterEventType.UPDATE_COMPLETE);
-            }
-        });
-        waitThread.setName(getClass().getCanonicalName());
-        String[] logLines = {
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: foo bar baz\n",
-                "11-11 00:00:00.001  123 321 I update_engine: Update successfully applied\n"
-            };
-        waitAndAssertTerminated(waitThread, logLines);
+    /** Test that events registered first are matched first */
+    public void testParseEventTypeMatchOrder() {
+        mParser.registerEventTrigger(
+                "update_engine",
+                "finished with ErrorCode::kSuccess",
+                UpdaterEventType.PATCH_COMPLETE);
+        mParser.registerEventTrigger(
+                "update_engine", "finished with ErrorCode", UpdaterEventType.ERROR);
+        String notError =
+                "11-11 00:00:00.001  123 321 I update_engine: finished with ErrorCode::kSuccess";
+        assertEquals(UpdaterEventType.PATCH_COMPLETE, mParser.parseEventType(notError));
     }
 
-    private void waitAndAssertTerminated(Thread waitThread, String[] logLines) throws Exception {
+    /** Helper class for testing waitForEvent */
+    private class WaitForEventHelper implements Runnable {
+        UpdaterEventType mReturnedEvent;
+        UpdaterEventType mExpectedEvent;
+        long mTimeoutMs;
+
+        public WaitForEventHelper(UpdaterEventType expectedEvent, long timeoutMs) {
+            mExpectedEvent = expectedEvent;
+            mTimeoutMs = timeoutMs;
+        }
+
+        @Override
+        public void run() {
+            mReturnedEvent = mParser.waitForEvent(mExpectedEvent, mTimeoutMs);
+        }
+
+        public UpdaterEventType getResult() {
+            return mReturnedEvent;
+        }
+    }
+
+    /** Test that waitForEvent returns once it sees a specific expect event. */
+    public void testWaitForEvent() throws Exception {
+        WaitForEventHelper waitTask =
+                new WaitForEventHelper(UpdaterEventType.UPDATE_COMPLETE, 60 * 1000);
+        Thread waitThread = new Thread(waitTask);
         waitThread.start();
+        feedMockPipe(LOGS_UPDATE_COMPLETE);
+        waitThread.join(SMALL_WAIT_TIME_MS);
+        assertEquals(Thread.State.TERMINATED, waitThread.getState());
+        assertEquals(UpdaterEventType.UPDATE_COMPLETE, waitTask.getResult());
+    }
+
+    /** Test that waitForEvent returns when it sees an update error. */
+    public void testWaitForEventError() throws Exception {
+        WaitForEventHelper waitTask =
+                new WaitForEventHelper(UpdaterEventType.UPDATE_COMPLETE, 60 * 1000);
+        Thread waitThread = new Thread(waitTask);
+        waitThread.start();
+        feedMockPipe(LOGS_ERROR);
+        waitThread.join(SMALL_WAIT_TIME_MS);
+        assertEquals(Thread.State.TERMINATED, waitThread.getState());
+        assertEquals(UpdaterEventType.ERROR, waitTask.getResult());
+    }
+
+    /** Test that waitForEvent honors the timeout. */
+    public void testWaitForEventTimeout() throws Exception {
+        WaitForEventHelper waitTask = new WaitForEventHelper(UpdaterEventType.UPDATE_COMPLETE, 0);
+        Thread waitThread = new Thread(waitTask);
+        waitThread.start();
+        feedMockPipe(LOGS_UPDATE_COMPLETE);
+        waitThread.join(SMALL_WAIT_TIME_MS);
+        assertEquals(Thread.State.TERMINATED, waitThread.getState());
+        assertEquals(UpdaterEventType.INFRA_TIMEOUT, waitTask.getResult());
+    }
+
+    /** Test that waitForEventAsync completes when it sees a specific expect event. */
+    public void testWaitForEventAsync() throws Exception {
+        AsyncUpdaterEvent event =
+                mParser.waitForEventAsync(UpdaterEventType.UPDATE_COMPLETE, 60 * 1000);
+        feedMockPipe(LOGS_UPDATE_COMPLETE);
+        synchronized (event) {
+            event.wait(SMALL_WAIT_TIME_MS);
+            assertTrue(event.isCompleted());
+            assertEquals(UpdaterEventType.UPDATE_COMPLETE, event.getResult());
+        }
+    }
+
+    /** Test that waitForEventAsync completes when it sees an update error. */
+    public void testWaitForEventAsyncError() throws Exception {
+        AsyncUpdaterEvent event =
+                mParser.waitForEventAsync(UpdaterEventType.UPDATE_COMPLETE, 60 * 1000);
+        feedMockPipe(LOGS_ERROR);
+        synchronized (event) {
+            event.wait(SMALL_WAIT_TIME_MS);
+            assertTrue(event.isCompleted());
+            assertEquals(UpdaterEventType.ERROR, event.getResult());
+        }
+    }
+
+    /** Test that waitForEventAsync honors the timeout. */
+    public void testWaitForEventAsyncTimeout() throws Exception {
+        AsyncUpdaterEvent event = mParser.waitForEventAsync(UpdaterEventType.UPDATE_COMPLETE, 0);
+        feedMockPipe(LOGS_UPDATE_COMPLETE);
+        synchronized (event) {
+            event.wait(SMALL_WAIT_TIME_MS);
+            assertTrue(event.isCompleted());
+            assertEquals(UpdaterEventType.INFRA_TIMEOUT, event.getResult());
+        }
+    }
+
+    private void feedMockPipe(String[] logLines) {
         for (String line : logLines) {
             try {
                 mMockPipe.write(line.getBytes());
@@ -162,10 +245,6 @@ public class LogcatUpdaterEventParserTest extends TestCase {
                 fail(e.getLocalizedMessage());
             }
         }
-        // Allow short time for thread to switch state.
-        RunUtil.getDefault().sleep(SHORT_WAIT_MS);
-        waitThread.join(5000);
-        assertEquals(Thread.State.TERMINATED, waitThread.getState());
     }
 }
 
