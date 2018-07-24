a49034d2af933fc6a04673d00887d630b7cc63b3
Julien Desprez <jdesprez@google.com>
2018-07-24 10:07:10 -0700

    Add parsing a fully finalized test record proto
    - Add the handling of a fully finalized test record proto in
    one go.
    - Add replaying of logs in sequence to track them.
    
    Test: unit tests
    Bug: 79990695
    Change-Id: If1954773d59f19e7f95eab7ebd88f87e18e1b743

--- src/com/android/tradefed/result/proto/ProtoResultParser.java
+++ src/com/android/tradefed/result/proto/ProtoResultParser.java
@@ -18,9 +18,15 @@ package com.android.tradefed.result.proto;
 import com.android.tradefed.invoker.IInvocationContext;
 import com.android.tradefed.invoker.InvocationContext;
 import com.android.tradefed.invoker.proto.InvocationContext.Context;
+import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.metrics.proto.MetricMeasurement.Metric;
+import com.android.tradefed.result.ILogSaverListener;
 import com.android.tradefed.result.ITestInvocationListener;
+import com.android.tradefed.result.LogDataType;
+import com.android.tradefed.result.LogFile;
 import com.android.tradefed.result.TestDescription;
+import com.android.tradefed.result.proto.LogFileProto.LogFileInfo;
+import com.android.tradefed.result.proto.TestRecordProto.ChildReference;
 import com.android.tradefed.result.proto.TestRecordProto.TestRecord;
 
 import com.google.protobuf.Any;
@@ -28,6 +34,8 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Timestamp;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
 /** Parser for the Tradefed results proto format. */
 public class ProtoResultParser {
@@ -38,6 +46,24 @@ public class ProtoResultParser {
     /** Ctor. */
     public ProtoResultParser(ITestInvocationListener listener) {
         mListener = listener;
+    }
+
+    /**
+     * Main entry function that takes the finalized completed proto and replay its results.
+     *
+     * @param finalProto The final {@link TestRecord} to be parsed.
+     */
+    public void processFinalizedProto(TestRecord finalProto) {
+        if (!finalProto.getParentTestRecordId().isEmpty()) {
+            throw new IllegalArgumentException("processFinalizedProto only expect a root proto.");
+        }
+
+        // Invocation Start
+        handleInvocationStart(finalProto);
+
+        evalProto(finalProto.getChildrenList(), false);
+        // Invocation End
+        handleInvocationEnded(finalProto);
     }
 
     /**
@@ -62,27 +88,66 @@ public class ProtoResultParser {
         }
     }
 
+    private void evalProto(List<ChildReference> children, boolean isInRun) {
+        for (ChildReference child : children) {
+            TestRecord childProto = child.getInlineTestRecord();
+            if (isInRun) {
+                // test case
+                String[] info = childProto.getTestRecordId().split("#");
+                TestDescription description = new TestDescription(info[0], info[1]);
+                mListener.testStarted(description, timeStampToMillis(childProto.getStartTime()));
+                handleTestCaseEnd(description, childProto);
+            } else {
+                boolean inRun = false;
+                if (childProto.hasDescription()) {
+                    // Module start
+                    handleModuleStart(childProto);
+                } else {
+                    // run start
+                    handleTestRunStart(childProto);
+                    inRun = true;
+                }
+                evalProto(childProto.getChildrenList(), inRun);
+                if (childProto.hasDescription()) {
+                    // Module end
+                    mListener.testModuleEnded();
+                } else {
+                    // run end
+                    handleTestRunEnd(childProto);
+                }
+            }
+        }
+    }
+
     /** Handles the root of the invocation: They have no parent record id. */
     private void handleRootProto(TestRecord rootProto) {
         if (rootProto.hasEndTime()) {
-            long elapsedTime =
-                    timeStampToMillis(rootProto.getEndTime())
-                            - timeStampToMillis(rootProto.getStartTime());
-            mListener.invocationEnded(elapsedTime);
+            handleInvocationEnded(rootProto);
         } else {
-            // invocation starting
-            Any anyDescription = rootProto.getDescription();
-            if (!anyDescription.is(Context.class)) {
-                throw new RuntimeException("Expected Any description of type Context");
-            }
-            try {
-                IInvocationContext context =
-                        InvocationContext.fromProto(anyDescription.unpack(Context.class));
-                mListener.invocationStarted(context);
-            } catch (InvalidProtocolBufferException e) {
-                throw new RuntimeException(e);
-            }
+            handleInvocationStart(rootProto);
         }
+    }
+
+    private void handleInvocationStart(TestRecord startInvocationProto) {
+        // invocation starting
+        Any anyDescription = startInvocationProto.getDescription();
+        if (!anyDescription.is(Context.class)) {
+            throw new RuntimeException("Expected Any description of type Context");
+        }
+        try {
+            IInvocationContext context =
+                    InvocationContext.fromProto(anyDescription.unpack(Context.class));
+            mListener.invocationStarted(context);
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleInvocationEnded(TestRecord endInvocationProto) {
+        long elapsedTime =
+                timeStampToMillis(endInvocationProto.getEndTime())
+                        - timeStampToMillis(endInvocationProto.getStartTime());
+        mListener.invocationEnded(elapsedTime);
     }
 
     /** Handles module level of the invocation: They have a Description for the module context. */
@@ -90,17 +155,21 @@ public class ProtoResultParser {
         if (moduleProto.hasEndTime()) {
             mListener.testModuleEnded();
         } else {
-            Any anyDescription = moduleProto.getDescription();
-            if (!anyDescription.is(Context.class)) {
-                throw new RuntimeException("Expected Any description of type Context");
-            }
-            try {
-                IInvocationContext moduleContext =
-                        InvocationContext.fromProto(anyDescription.unpack(Context.class));
-                mListener.testModuleStarted(moduleContext);
-            } catch (InvalidProtocolBufferException e) {
-                throw new RuntimeException(e);
-            }
+            handleModuleStart(moduleProto);
+        }
+    }
+
+    private void handleModuleStart(TestRecord moduleProto) {
+        Any anyDescription = moduleProto.getDescription();
+        if (!anyDescription.is(Context.class)) {
+            throw new RuntimeException("Expected Any description of type Context");
+        }
+        try {
+            IInvocationContext moduleContext =
+                    InvocationContext.fromProto(anyDescription.unpack(Context.class));
+            mListener.testModuleStarted(moduleContext);
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -108,58 +177,92 @@ public class ProtoResultParser {
     private void handleTestRun(TestRecord runProto) {
         // If the proto end-time is present we are evaluating the end of a test run.
         if (runProto.hasEndTime()) {
-            // If we find debugging information, the test run failed and we reflect it.
-            if (runProto.hasDebugInfo()) {
-                mListener.testRunFailed(runProto.getDebugInfo().getErrorMessage());
-            }
-            long elapsedTime =
-                    timeStampToMillis(runProto.getEndTime())
-                            - timeStampToMillis(runProto.getStartTime());
-            HashMap<String, Metric> metrics = new HashMap<>(runProto.getMetrics());
-            mListener.testRunEnded(elapsedTime, metrics);
+            handleTestRunEnd(runProto);
             mCurrentRunName = null;
         } else {
             // If the end-time is not populated yet we are dealing with the start of a run.
             mCurrentRunName = runProto.getTestRecordId();
-            mListener.testRunStarted(
-                    runProto.getTestRecordId(), (int) runProto.getNumExpectedChildren());
+            handleTestRunStart(runProto);
         }
     }
 
+    private void handleTestRunStart(TestRecord runProto) {
+        mListener.testRunStarted(
+                runProto.getTestRecordId(), (int) runProto.getNumExpectedChildren());
+    }
+
+    private void handleTestRunEnd(TestRecord runProto) {
+        // If we find debugging information, the test run failed and we reflect it.
+        if (runProto.hasDebugInfo()) {
+            mListener.testRunFailed(runProto.getDebugInfo().getErrorMessage());
+        }
+        handleLogs(runProto);
+        long elapsedTime =
+                timeStampToMillis(runProto.getEndTime())
+                        - timeStampToMillis(runProto.getStartTime());
+        HashMap<String, Metric> metrics = new HashMap<>(runProto.getMetrics());
+        mListener.testRunEnded(elapsedTime, metrics);
+    }
+
     /** Handles the test cases level of the invocation. */
-    public void handleTestCase(TestRecord testcaseProto) {
+    private void handleTestCase(TestRecord testcaseProto) {
         String[] info = testcaseProto.getTestRecordId().split("#");
         TestDescription description = new TestDescription(info[0], info[1]);
         if (testcaseProto.hasEndTime()) {
-            switch (testcaseProto.getStatus()) {
-                case FAIL:
-                    mListener.testFailed(
-                            description, testcaseProto.getDebugInfo().getErrorMessage());
-                    break;
-                case ASSUMPTION_FAILURE:
-                    mListener.testAssumptionFailure(
-                            description, testcaseProto.getDebugInfo().getTrace());
-                    break;
-                case IGNORED:
-                    mListener.testIgnored(description);
-                    break;
-                case PASS:
-                    break;
-                default:
-                    throw new RuntimeException(
-                            String.format(
-                                    "Received unexpected test status %s.",
-                                    testcaseProto.getStatus()));
-            }
-            HashMap<String, Metric> metrics = new HashMap<>(testcaseProto.getMetrics());
-            mListener.testEnded(
-                    description, timeStampToMillis(testcaseProto.getEndTime()), metrics);
+            handleTestCaseEnd(description, testcaseProto);
         } else {
             mListener.testStarted(description, timeStampToMillis(testcaseProto.getStartTime()));
         }
     }
 
+    private void handleTestCaseEnd(TestDescription description, TestRecord testcaseProto) {
+        switch (testcaseProto.getStatus()) {
+            case FAIL:
+                mListener.testFailed(description, testcaseProto.getDebugInfo().getErrorMessage());
+                break;
+            case ASSUMPTION_FAILURE:
+                mListener.testAssumptionFailure(
+                        description, testcaseProto.getDebugInfo().getTrace());
+                break;
+            case IGNORED:
+                mListener.testIgnored(description);
+                break;
+            case PASS:
+                break;
+            default:
+                throw new RuntimeException(
+                        String.format(
+                                "Received unexpected test status %s.", testcaseProto.getStatus()));
+        }
+        handleLogs(testcaseProto);
+        HashMap<String, Metric> metrics = new HashMap<>(testcaseProto.getMetrics());
+        mListener.testEnded(description, timeStampToMillis(testcaseProto.getEndTime()), metrics);
+    }
+
     private long timeStampToMillis(Timestamp stamp) {
         return stamp.getSeconds() * 1000L + (stamp.getNanos() / 1000000L);
+    }
+
+    private void handleLogs(TestRecord proto) {
+        if (!(mListener instanceof ILogSaverListener)) {
+            return;
+        }
+        ILogSaverListener logger = (ILogSaverListener) mListener;
+        for (Entry<String, Any> entry : proto.getArtifacts().entrySet()) {
+            try {
+                LogFileInfo info = entry.getValue().unpack(LogFileInfo.class);
+                LogFile file =
+                        new LogFile(
+                                info.getPath(),
+                                info.getUrl(),
+                                info.getIsCompressed(),
+                                LogDataType.valueOf(info.getLogType()),
+                                info.getSize());
+                logger.logAssociation(entry.getKey(), file);
+            } catch (InvalidProtocolBufferException e) {
+                CLog.e("Couldn't unpack %s as a LogFileInfo", entry.getKey());
+                CLog.e(e);
+            }
+        }
     }
 }
