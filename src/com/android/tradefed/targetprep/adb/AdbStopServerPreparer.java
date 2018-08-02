d1a4adf244376a06cf8edcbf14a1028ecf8c15c6
Julien Desprez <jdesprez@google.com>
2018-08-02 09:09:34 -0700

    Add support for python unit tests and adb use cases
    - Handle pure python unit tests in par files.
    - Handle the adb required setup in a preparer for adb
    testing.
    
    Test: tf unit tests
    atest adb_integration_test_adb with a temporary AndroidTest.xml
    http://sponge.corp.google.com/invocation?tab=Test+Cases&show=FAILED&show=INTERNAL_ERROR&id=3d282e52-4cbe-434c-9d16-36323902419c
    Bug: 112104122
    
    Change-Id: I95f3a3ac9b41543d9f332b20c97716d5c6d949b0

--- src/com/android/tradefed/targetprep/adb/AdbStopServerPreparer.java
+++ src/com/android/tradefed/targetprep/adb/AdbStopServerPreparer.java
@@ -15,24 +15,42 @@
  */
 package com.android.tradefed.targetprep.adb;
 
+import com.android.annotations.VisibleForTesting;
 import com.android.tradefed.build.IBuildInfo;
 import com.android.tradefed.config.GlobalConfiguration;
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.IDeviceManager;
 import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.targetprep.BaseTargetPreparer;
 import com.android.tradefed.targetprep.BuildError;
 import com.android.tradefed.targetprep.ITargetCleaner;
+import com.android.tradefed.targetprep.SemaphoreTokenTargetPreparer;
 import com.android.tradefed.targetprep.TargetSetupError;
+import com.android.tradefed.util.CommandResult;
+import com.android.tradefed.util.CommandStatus;
+import com.android.tradefed.util.FileUtil;
 import com.android.tradefed.util.IRunUtil;
 import com.android.tradefed.util.RunUtil;
 
-/** Target preparer to stop adb server on the host before and after running adb tests. */
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Target preparer to stop adb server on the host before and after running adb tests.
+ *
+ * <p>This preparer should be used with care as it stops and restart adb on the hosts. It should
+ * usually be tight with {@link SemaphoreTokenTargetPreparer} to avoid other tests from running at
+ * the same time.
+ */
 public class AdbStopServerPreparer extends BaseTargetPreparer implements ITargetCleaner {
 
-    private IRunUtil mRunUtil = RunUtil.getDefault();
-
+    private static final String PATH = "PATH";
     private static final long CMD_TIMEOUT = 60000L;
+    private static final String ANDROID_HOST_OUT = "ANDROID_HOST_OUT";
+
+    private IRunUtil mRunUtil;
+    private File mTmpDir;
 
     /** {@inheritDoc} */
     @Override
@@ -42,29 +60,89 @@ public class AdbStopServerPreparer extends BaseTargetPreparer implements ITarget
         getDeviceManager().stopAdbBridge();
 
         // Kill the default adb server
-        mRunUtil.runTimedCmd(CMD_TIMEOUT, "adb", "kill-server");
-        // Wait 1000ms for "adb kill-server" to finish. See b/37104408
-        mRunUtil.sleep(1 * 1000);
+        getRunUtil().runTimedCmd(CMD_TIMEOUT, "adb", "kill-server");
+
+        File adb = null;
+        if (System.getenv(ANDROID_HOST_OUT) != null) {
+            String hostOut = System.getenv(ANDROID_HOST_OUT);
+            adb = new File(hostOut, "bin/adb");
+            if (adb.exists()) {
+                adb.setExecutable(true);
+            } else {
+                adb = null;
+            }
+        }
+
+        if (adb == null && buildInfo.getFile("adb") != null) {
+            adb = buildInfo.getFile("adb");
+            adb = renameAdbBinary(adb);
+        }
+
+        if (adb != null) {
+            CLog.d("Restarting adb from %s", adb.getAbsolutePath());
+            IRunUtil restartAdb = createRunUtil();
+            restartAdb.setEnvVariable(PATH, adb.getAbsolutePath());
+            CommandResult result =
+                    restartAdb.runTimedCmd(CMD_TIMEOUT, adb.getAbsolutePath(), "start-server");
+            if (!CommandStatus.SUCCESS.equals(result.getStatus())) {
+                throw new TargetSetupError(
+                        "Failed to restart adb with the build info one.",
+                        device.getDeviceDescriptor());
+            }
+        } else {
+            getRunUtil().runTimedCmd(CMD_TIMEOUT, "adb", "start-server");
+            throw new TargetSetupError(
+                    "Could not find a new version of adb to tests.", device.getDeviceDescriptor());
+        }
     }
 
     /** {@inheritDoc} */
     @Override
     public void tearDown(ITestDevice device, IBuildInfo buildInfo, Throwable e)
             throws DeviceNotAvailableException {
+        FileUtil.recursiveDelete(mTmpDir);
         // Kill the test adb server
-        mRunUtil.runTimedCmd(CMD_TIMEOUT, "adb", "kill-server");
-        // Wait 1000ms for "adb kill-server" to finish. See b/37104408
-        mRunUtil.sleep(1 * 1000);
-        // Restart the default adb server found in system PATH. Ensure adb server is already
-        // running the next time TF executes a command over adb bridge. Otherwise adb bridge may
-        // see a "Connection refused" exception.
-        mRunUtil.runTimedCmd(CMD_TIMEOUT, "adb", "start-server");
-        mRunUtil.sleep(1 * 1000);
-
+        getRunUtil().runTimedCmd(CMD_TIMEOUT, "adb", "kill-server");
+        // Restart the one from the parent PATH (original one)
+        getRunUtil().runTimedCmd(CMD_TIMEOUT, "adb", "start-server");
+        // Restart device manager monitor
         getDeviceManager().restartAdbBridge();
     }
 
+    @VisibleForTesting
     IDeviceManager getDeviceManager() {
         return GlobalConfiguration.getDeviceManagerInstance();
+    }
+
+    @VisibleForTesting
+    IRunUtil createRunUtil() {
+        return new RunUtil();
+    }
+
+    private IRunUtil getRunUtil() {
+        if (mRunUtil == null) {
+            mRunUtil = createRunUtil();
+        }
+        return mRunUtil;
+    }
+
+    private File renameAdbBinary(File originalAdb) {
+        try {
+            mTmpDir = FileUtil.createTempDir("adb");
+        } catch (IOException e) {
+            CLog.e("Cannot create temp directory");
+            FileUtil.recursiveDelete(mTmpDir);
+            return null;
+        }
+        File renamedAdbBinary = new File(mTmpDir, "adb");
+        if (!originalAdb.renameTo(renamedAdbBinary)) {
+            CLog.e("Cannot rename adb binary");
+            return null;
+        }
+        if (!renamedAdbBinary.setExecutable(true)) {
+            CLog.e("Cannot set adb binary executable");
+            return null;
+        }
+        return renamedAdbBinary;
     }
 }
