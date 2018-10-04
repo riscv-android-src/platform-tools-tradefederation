de354c99df8558c6cca59c88b880feb3fff997a1
Julien Desprez <jdesprez@google.com>
2018-10-04 14:17:44 -0700

    Explicitly set sandbox interrupt to false
    Test: unit tests
    Bug: None
    Change-Id: I6e84f25ad5262ce6b0710ef493bc15b84d7687c7

--- src/com/android/tradefed/sandbox/TradefedSandbox.java
+++ src/com/android/tradefed/sandbox/TradefedSandbox.java
@@ -107,6 +107,7 @@ public class TradefedSandbox implements ISandbox {
         }
 
         long timeout = config.getCommandOptions().getInvocationTimeout();
+        mRunUtil.allowInterrupt(false);
         CommandResult result =
                 mRunUtil.runTimedCmd(timeout, mStdout, mStderr, mCmdArgs.toArray(new String[0]));
         // Log stdout and stderr
