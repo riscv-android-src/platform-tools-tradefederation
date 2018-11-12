0b27ca7dc7862fcaca37a8417e5f16d5ed2fbcdd
Julien Desprez <jdesprez@google.com>
2018-11-12 10:15:04 -0800

    Cherry-pick: Add GceManager to AOSP
    Test: unit tests
    Bug: 119185816
    Change-Id: I4f9a73805f73f39b08d7dcd12651ce6a0f664460
    Merged-In: Idec75ca6a9a4ef7bc742c260c5a98b307e6bc3cf

---
+++ src/com/android/tradefed/device/cloud/GceRemoteCmdFormatter.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tradefed.device.cloud;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Utility class to format commands to reach a remote gce device. */
+public class GceRemoteCmdFormatter {
+
+    /**
+     * Utility to create a ssh command for a gce device based on some parameters.
+     *
+     * @param sshKey the ssh key {@link File}.
+     * @param extraOptions a List of {@link String} that can be added for extra ssh options. can be
+     *     null.
+     * @param hostName the hostname where to connect to the gce device.
+     * @param command the actual command to run on the gce device.
+     * @return a list representing the ssh command for a gce device.
+     */
+    public static List<String> getSshCommand(
+            File sshKey,
+            List<String> extraOptions,
+            String user,
+            String hostName,
+            String... command) {
+        List<String> cmd = new ArrayList<>();
+        cmd.add("ssh");
+        cmd.add("-o");
+        cmd.add("UserKnownHostsFile=/dev/null");
+        cmd.add("-o");
+        cmd.add("StrictHostKeyChecking=no");
+        cmd.add("-o");
+        cmd.add("ServerAliveInterval=10");
+        cmd.add("-i");
+        cmd.add(sshKey.getAbsolutePath());
+        if (extraOptions != null) {
+            for (String op : extraOptions) {
+                cmd.add(op);
+            }
+        }
+        cmd.add(user + "@" + hostName);
+        for (String cmdOption : command) {
+            cmd.add(cmdOption);
+        }
+        return cmd;
+    }
+
+    /**
+     * Utility to create a scp command to fetch a file from a remote gce device.
+     *
+     * @param sshKey the ssh key {@link File}.
+     * @param extraOptions a List of {@link String} that can be added for extra ssh options. can be
+     *     null.
+     * @param hostName the hostname where to connect to the gce device.
+     * @param remoteFile the file to be fetched on the remote gce device.
+     * @param localFile the local file where to put the remote file.
+     * @return a list representing the scp command for a gce device.
+     */
+    public static List<String> getScpCommand(
+            File sshKey,
+            List<String> extraOptions,
+            String user,
+            String hostName,
+            String remoteFile,
+            String localFile) {
+        List<String> cmd = new ArrayList<>();
+        cmd.add("scp");
+        cmd.add("-o");
+        cmd.add("UserKnownHostsFile=/dev/null");
+        cmd.add("-o");
+        cmd.add("StrictHostKeyChecking=no");
+        cmd.add("-o");
+        cmd.add("ServerAliveInterval=10");
+        cmd.add("-i");
+        cmd.add(sshKey.getAbsolutePath());
+        if (extraOptions != null) {
+            for (String op : extraOptions) {
+                cmd.add(op);
+            }
+        }
+        cmd.add(String.format("%s@%s:%s", user, hostName, remoteFile));
+        cmd.add(localFile);
+        return cmd;
+    }
+}
