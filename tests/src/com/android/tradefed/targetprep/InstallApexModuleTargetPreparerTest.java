47871b289fbcfcc18dd460f453ef714c88370a0c
Chen Zhu <chenzhu@google.com>
2018-12-06 15:08:34 -0800

    Add initial InstallApexModuleTargetPreparer to install, activate apex module and verify installation and activation before running module specific tests (e.g., cts test).
    Test: cd tests; mma; ./run_tradefed_tests.sh
    Test: atest apex_e2e_tests (passed with the following test config)
    
    <configuration description="Config for apex install/uninstall test cases">
        <option name="test-suite-tag" value="apex_e2e_tests" />
        <option name="test-suite-tag" value="apct" />
        <target_preparer class="com.android.tradefed.targetprep.DisableSELinuxTargetPreparer"/>
        <target_preparer class="com.android.tradefed.targetprep.RootTargetPreparer"/>
        <target_preparer class="com.android.tradefed.targetprep.InstallApexModuleTargetPreparer" >
             <option name="test-file-name" value="test.apex" />
             <option name="apex-packageName" value="com.android.apex.test" />
             <option name="apex-version" value="1" />
        </target_preparer>
    </configuration>
    
    Bug: 115880045
    Bug: 120048749
    Change-Id: I08e638d79a3d05bd84ace4832b10a43272d92b5f

---
+++ tests/src/com/android/tradefed/targetprep/InstallApexModuleTargetPreparerTest.java
@@ -0,0 +1,239 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tradefed.targetprep;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.android.tradefed.build.IBuildInfo;
+import com.android.tradefed.command.remote.DeviceDescriptor;
+import com.android.tradefed.config.OptionSetter;
+import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.device.ITestDevice.ApexInfo;
+import com.android.tradefed.util.CommandResult;
+import com.android.tradefed.util.CommandStatus;
+import com.android.tradefed.util.FileUtil;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+/** Unit test for {@link InstallApexModuleTargetPreparer} */
+@RunWith(JUnit4.class)
+public class InstallApexModuleTargetPreparerTest {
+
+    private static final String SERIAL = "serial";
+    private InstallApexModuleTargetPreparer mInstallApexModuleTargetPreparer;
+    private IBuildInfo mMockBuildInfo;
+    private ITestDevice mMockDevice;
+    private OptionSetter mSetter;
+    private File mFakeApex;
+    private static final String APEX_PACKAGE_NAME = "com.android.FAKE_APEX_PACKAGE_NAME";
+    private static final String APEX_PACKAGE_KEYWORD = "FAKE_APEX_PACKAGE_NAME";
+    private static final String APEX_VERSION = "1";
+    private static final String APEX_NAME = "fakeApex.apex";
+    private static final String REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND =
+            "rm -rf /data/apex/*FAKE_APEX_PACKAGE_NAME*";
+
+    @Before
+    public void setUp() throws Exception {
+        mFakeApex = FileUtil.createTempFile("fakeApex", ".apex");
+        mInstallApexModuleTargetPreparer =
+                new InstallApexModuleTargetPreparer() {
+                    @Override
+                    protected String getModuleKeywordFromApexPackageName(String packageName) {
+                        return APEX_PACKAGE_KEYWORD;
+                    }
+
+                    @Override
+                    protected File getLocalPathForFilename(
+                            IBuildInfo buildInfo, String apexFileName, ITestDevice device)
+                            throws TargetSetupError {
+                        return mFakeApex;
+                    }
+
+                    @Override
+                    protected String parsePackageName(
+                            File testApexFile, DeviceDescriptor deviceDescriptor) {
+                        return APEX_PACKAGE_NAME;
+                    }
+                };
+        mInstallApexModuleTargetPreparer.addTestFileName(APEX_NAME);
+        mSetter = new OptionSetter(mInstallApexModuleTargetPreparer);
+        mSetter.setOptionValue("apex-packageName", APEX_PACKAGE_NAME);
+        mSetter.setOptionValue("apex-version", APEX_VERSION);
+
+        mMockDevice = EasyMock.createMock(ITestDevice.class);
+        mMockBuildInfo = EasyMock.createMock(IBuildInfo.class);
+        EasyMock.expect(mMockDevice.getSerialNumber()).andStubReturn(SERIAL);
+        EasyMock.expect(mMockDevice.getDeviceDescriptor()).andStubReturn(null);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FileUtil.deleteFile(mFakeApex);
+    }
+
+    @Test
+    public void testSetupSuccess_removeExistingStagedApexSuccess() throws Exception {
+        CommandResult resRemoveExistingApex = new CommandResult();
+        resRemoveExistingApex.setStatus(CommandStatus.SUCCESS);
+        EasyMock.expect(mMockDevice.executeShellV2Command(REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND))
+                .andReturn(resRemoveExistingApex)
+                .once();
+        mockSuccessfulInstallPackageAndReboot();
+        Set<ApexInfo> activatedApex = new HashSet<ApexInfo>();
+        activatedApex.add(new ApexInfo("com.android.FAKE_APEX_PACKAGE_NAME", 1));
+        EasyMock.expect(mMockDevice.getActiveApexes()).andReturn(activatedApex);
+
+        EasyMock.replay(mMockBuildInfo, mMockDevice);
+        mInstallApexModuleTargetPreparer.setUp(mMockDevice, mMockBuildInfo);
+        EasyMock.verify(mMockBuildInfo, mMockDevice);
+    }
+
+    @Test
+    public void testSetupFail_removeExistingStagedApexFail() throws Exception {
+        CommandResult resRemoveExistingApex = new CommandResult();
+        resRemoveExistingApex.setStatus(CommandStatus.FAILED);
+        EasyMock.expect(mMockDevice.executeShellV2Command(REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND))
+                .andReturn(resRemoveExistingApex);
+        try {
+            EasyMock.replay(mMockBuildInfo, mMockDevice);
+            mInstallApexModuleTargetPreparer.setUp(mMockDevice, mMockBuildInfo);
+            fail("Should have thrown a TargetSetupError.");
+        } catch (TargetSetupError expected) {
+            assertTrue(expected.getMessage().contains("Failed to clean up data/apex on device"));
+        } finally {
+            EasyMock.verify(mMockBuildInfo, mMockDevice);
+        }
+    }
+
+    @Test
+    public void testSetupSuccess_getActivatedPackageSuccess() throws Exception {
+        CommandResult resRemoveExistingApex = new CommandResult();
+        resRemoveExistingApex.setStatus(CommandStatus.SUCCESS);
+        EasyMock.expect(mMockDevice.executeShellV2Command(REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND))
+                .andReturn(resRemoveExistingApex)
+                .once();
+        mockSuccessfulInstallPackageAndReboot();
+        Set<ApexInfo> activatedApex = new HashSet<ApexInfo>();
+        activatedApex.add(new ApexInfo("com.android.FAKE_APEX_PACKAGE_NAME", 1));
+        EasyMock.expect(mMockDevice.getActiveApexes()).andReturn(activatedApex);
+
+        EasyMock.replay(mMockBuildInfo, mMockDevice);
+        mInstallApexModuleTargetPreparer.setUp(mMockDevice, mMockBuildInfo);
+        EasyMock.verify(mMockBuildInfo, mMockDevice);
+    }
+
+    @Test
+    public void testSetupFail_getActivatedPackageFail() throws Exception {
+        CommandResult resRemoveExistingApex = new CommandResult();
+        resRemoveExistingApex.setStatus(CommandStatus.SUCCESS);
+        EasyMock.expect(mMockDevice.executeShellV2Command(REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND))
+                .andReturn(resRemoveExistingApex)
+                .once();
+        mockSuccessfulInstallPackageAndReboot();
+        EasyMock.expect(mMockDevice.getActiveApexes()).andReturn(new HashSet<ApexInfo>());
+
+        try {
+            EasyMock.replay(mMockBuildInfo, mMockDevice);
+            mInstallApexModuleTargetPreparer.setUp(mMockDevice, mMockBuildInfo);
+            fail("Should have thrown a TargetSetupError.");
+        } catch (TargetSetupError expected) {
+            assertTrue(
+                    expected.getMessage()
+                            .contains("Failed to retrieve activated apex on device serial."));
+        } finally {
+            EasyMock.verify(mMockBuildInfo, mMockDevice);
+        }
+    }
+
+    @Test
+    public void testSetupFail_apexActivationFail() throws Exception {
+        CommandResult resRemoveExistingApex = new CommandResult();
+        resRemoveExistingApex.setStatus(CommandStatus.SUCCESS);
+        EasyMock.expect(mMockDevice.executeShellV2Command(REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND))
+                .andReturn(resRemoveExistingApex)
+                .once();
+        mockSuccessfulInstallPackageAndReboot();
+        Set<ApexInfo> activatedApex = new HashSet<ApexInfo>();
+        activatedApex.add(new ApexInfo("com.android.FAKE_APEX_PACKAGE_NAME_TO_FAIL", 1));
+        EasyMock.expect(mMockDevice.getActiveApexes()).andReturn(activatedApex);
+
+        try {
+            EasyMock.replay(mMockBuildInfo, mMockDevice);
+            mInstallApexModuleTargetPreparer.setUp(mMockDevice, mMockBuildInfo);
+            fail("Should have thrown a TargetSetupError.");
+        } catch (TargetSetupError expected) {
+            assertTrue(
+                    expected.getMessage()
+                            .contains(
+                                    String.format(
+                                            "Failed to activate %s on device %s.",
+                                            mInstallApexModuleTargetPreparer
+                                                    .getTestsFileName()
+                                                    .toString(),
+                                            mMockDevice.getSerialNumber())));
+        } finally {
+            EasyMock.verify(mMockBuildInfo, mMockDevice);
+        }
+    }
+
+    @Test
+    public void testTearDown() throws Exception {
+        CommandResult resRemoveExistingApex = new CommandResult();
+        resRemoveExistingApex.setStatus(CommandStatus.SUCCESS);
+        EasyMock.expect(mMockDevice.executeShellV2Command(REMOVE_EXISITING_APEX_UNDER_DATA_COMMAND))
+                .andReturn(resRemoveExistingApex)
+                .once();
+        mMockDevice.reboot();
+        EasyMock.expectLastCall();
+        EasyMock.replay(mMockBuildInfo, mMockDevice);
+        mInstallApexModuleTargetPreparer.tearDown(mMockDevice, mMockBuildInfo, null);
+        EasyMock.verify(mMockBuildInfo, mMockDevice);
+    }
+
+    @Test
+    public void testGetModuleKeyword() {
+        mInstallApexModuleTargetPreparer = new InstallApexModuleTargetPreparer();
+        final String testApex1PackageName = "com.android.foo";
+        final String testApex2PackageName = "com.android.bar_test";
+        assertEquals(
+                "foo",
+                mInstallApexModuleTargetPreparer.getModuleKeywordFromApexPackageName(
+                        testApex1PackageName));
+        assertEquals(
+                "bar_test",
+                mInstallApexModuleTargetPreparer.getModuleKeywordFromApexPackageName(
+                        testApex2PackageName));
+    }
+
+    private void mockSuccessfulInstallPackageAndReboot() throws Exception {
+        EasyMock.expect(mMockDevice.installPackage((File) EasyMock.anyObject(), EasyMock.eq(true)))
+                .andReturn(null)
+                .once();
+        mMockDevice.reboot();
+        EasyMock.expectLastCall().once();
+    }
+}
