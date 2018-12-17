a205d51522c76c3a9dca26daa2e037e8e4301ee8
Julien Desprez <jdesprez@google.com>
2018-12-17 09:50:28 -0800

    Implement the telephony token provider
    Suport SIM_CARD and UICC_SIM_CARD.
    
    Test: tf/cts unit tests
    ./cts-tradefed run cts-dev -m CtsNetTestCases --shard-count 2 (with and
    without sim card)
    ./cts-tradefed run cts-dev -m CtsCarrierApiTestCases --shard-count 2
    (with and without uicc sim card)
    Bug: 69367250
    
    Change-Id: Ia8c6217f5da42a22473a2e2fd5b7e6b4d5d8ef90

--- src/com/android/tradefed/invoker/shard/token/TelephonyTokenProvider.java
+++ src/com/android/tradefed/invoker/shard/token/TelephonyTokenProvider.java
@@ -15,14 +15,52 @@
  */
 package com.android.tradefed.invoker.shard.token;
 
+import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.device.StubDevice;
+import com.android.tradefed.device.helper.TelephonyHelper;
+import com.android.tradefed.device.helper.TelephonyHelper.SimCardInformation;
+import com.android.tradefed.log.LogUtil.CLog;
 
 /** Token provider for telephony related tokens. */
 public class TelephonyTokenProvider implements ITokenProvider {
 
     @Override
     public boolean hasToken(ITestDevice device, TokenProperty token) {
-        // TODO: To be implemented.
+        if (device.getIDevice() instanceof StubDevice) {
+            return false;
+        }
+        try {
+            SimCardInformation info = TelephonyHelper.getSimInfo(device);
+            if (info == null || !info.mHasTelephonySupport) {
+                CLog.e("SimcardInfo: %s", info);
+                return false;
+            }
+            switch (token) {
+                case SIM_CARD:
+                    // 5 is sim state ready
+                    if ("5".equals(info.mSimState)) {
+                        return true;
+                    }
+                    CLog.w(
+                            "%s cannot run with token '%s' - Sim info: %s",
+                            device.getSerialNumber(), token, info);
+                    return false;
+                case UICC_SIM_CARD:
+                    if (info.mCarrierPrivileges) {
+                        return true;
+                    }
+                    CLog.w(
+                            "%s cannot run with token '%s' - Sim info: %s",
+                            device.getSerialNumber(), token, info);
+                    return false;
+                default:
+                    CLog.w("Token '%s' doesn't match any TelephonyTokenProvider tokens.", token);
+                    return false;
+            }
+        } catch (DeviceNotAvailableException e) {
+            CLog.e("Ignoring DNAE: %s", e);
+        }
         return false;
     }
 }
