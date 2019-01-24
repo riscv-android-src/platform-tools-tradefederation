cd95a85c3203a97fa3b6b89b9682734bf956fc73
Xing Dai <xingdai@google.com>
2019-01-24 13:17:41 -0800

    [fix] Add a createCredential function.
    Add createCredentail function in Google ApiClientUtil that will try
    different keys and fallbackes.
    
    Bug: 123362588
    Test: manually.
    Change-Id: Ib4842210d47099843a8b11ac77851a986445f228

--- tests/src/com/android/tradefed/UnitTests.java
+++ tests/src/com/android/tradefed/UnitTests.java
@@ -279,6 +279,7 @@ import com.android.tradefed.util.FakeTestsZipFolderTest;
 import com.android.tradefed.util.FileUtilTest;
 import com.android.tradefed.util.FixedByteArrayOutputStreamTest;
 import com.android.tradefed.util.GCSFileDownloaderTest;
+import com.android.tradefed.util.GoogleApiClientUtilTest;
 import com.android.tradefed.util.HprofAllocSiteParserTest;
 import com.android.tradefed.util.JUnitXmlParserTest;
 import com.android.tradefed.util.KeyguardControllerStateTest;
@@ -684,6 +685,7 @@ import org.junit.runners.Suite.SuiteClasses;
     FileUtilTest.class,
     FixedByteArrayOutputStreamTest.class,
     GCSFileDownloaderTest.class,
+    GoogleApiClientUtilTest.class,
     HprofAllocSiteParserTest.class,
     JUnitXmlParserTest.class,
     KeyguardControllerStateTest.class,
