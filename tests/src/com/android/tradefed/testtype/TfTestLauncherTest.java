c0e985fce2ea91b1e9181d064f1ba4d2ed8f0ff5
Guang Zhu <guangzhu@google.com>
2019-08-16 18:51:21 -0700

    use java binary that launched current process for subprocess
    As a first step of consolidating use of java binary in TF, we
    are moving away from using the java binary from path; instead
    we are using the same java binary that launched current process
    for all subprocess test execution needs.
    
    In the future, some of these use cases will be changed to be
    using java binary based on the API level where subprocess
    artifacts are obtained (or they'll come pre-packaged with java
    binaries).
    
    Bug: 122862400
    Test: existing unit tests
    Change-Id: I9dc6907cba6af65335ad7e490bb67fbc5b578657

--- tests/src/com/android/tradefed/testtype/TfTestLauncherTest.java
+++ tests/src/com/android/tradefed/testtype/TfTestLauncherTest.java
@@ -91,7 +91,7 @@ public class TfTestLauncherTest {
                                 EasyMock.anyLong(),
                                 (FileOutputStream) EasyMock.anyObject(),
                                 (FileOutputStream) EasyMock.anyObject(),
-                                EasyMock.eq("java"),
+                                EasyMock.endsWith("/java"),
                                 (String) EasyMock.anyObject(),
                                 EasyMock.eq("--add-opens=java.base/java.nio=ALL-UNNAMED"),
                                 EasyMock.eq("-cp"),
