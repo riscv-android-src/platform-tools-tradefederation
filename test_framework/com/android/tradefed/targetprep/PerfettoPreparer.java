2e2f1107d091095e8c81f8c3e41b2eb7e6aa3f26
Gopinath Elanchezhian <gelanchezhian@google.com>
2019-12-06 14:53:52 -0800

    Remove perfetto preparer text proto support.
    Use text proto option in the perfetto tool directly instead.
    
    Bug: b/139092655
    
    Test: PerfettoPreparerTest
    Change-Id: Ie14cc0aa479146a1ac324c6e07c462debaee139d

--- test_framework/com/android/tradefed/targetprep/PerfettoPreparer.java
+++ test_framework/com/android/tradefed/targetprep/PerfettoPreparer.java
@@ -21,22 +21,12 @@ import com.android.tradefed.config.OptionClass;
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.log.LogUtil.CLog;
-import com.android.tradefed.util.FileUtil;
 
-import com.google.protobuf.TextFormat;
-import com.google.protobuf.TextFormat.ParseException;
-
-import perfetto.protos.PerfettoConfig.TraceConfig;
-
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Perfetto preparer pushes the config file in to the device at a standard location to which
- * perfetto have access. If the encoded version of perfetto is passed it will be directly pushed on
- * to the device otherwise it will be converted to binary version before it is pushed in to the
- * device. Perfetto can use only binary version of the config file.
+ * perfetto have access to.
  */
 @OptionClass(alias = "perfetto-preparer")
 public class PerfettoPreparer extends BaseTargetPreparer implements ITargetCleaner {
@@ -55,56 +45,18 @@ public class PerfettoPreparer extends BaseTargetPreparer implements ITargetClean
             description = "Full path to the binary version of perfetto config file.")
     private File mBinaryPerfettoConfigFile = null;
 
-    @Option(
-            name = "textproto-perfetto-config",
-            description = "Full path to the textproto version of perfetto config file.")
-    private File mTextprotoPerfettoConfigFile = null;
 
     @Override
     public void setUp(ITestDevice device, IBuildInfo buildInfo)
             throws TargetSetupError, BuildError, DeviceNotAvailableException {
 
-        // If the encoded version is passed, push it to the device directly and use it.
-        // Otherwise if the text format version is passed then convert it to the encoded
-        // format and push it to the device under /data/misc/perfetto-traces/
-
-        if (mBinaryPerfettoConfigFile != null && mTextprotoPerfettoConfigFile != null) {
-            CLog.w("Both binary and text proto options are set. Using binary option.");
-        }
+        // If the encoded version is passed, push it to the device directly
+        // under /data/misc/perfetto-traces/ and use it.
         if (mBinaryPerfettoConfigFile != null) {
             if (!mBinaryPerfettoConfigFile.exists()) {
                 fail("Failed to find the local binary perfetto file", null, device);
             } else if (!device.pushFile(mBinaryPerfettoConfigFile, DEVICE_CONFIG_PATH)) {
                 fail("Failed to push the binary perfetto file", null, device);
-            }
-        } else if (mTextprotoPerfettoConfigFile != null) {
-            String configStr = null;
-            try {
-                configStr = FileUtil.readStringFromFile(mTextprotoPerfettoConfigFile);
-            } catch (IOException e) {
-                fail("Failed to read the perfetto text file", e, device);
-            }
-            TraceConfig.Builder builder = TraceConfig.newBuilder();
-            try {
-                TextFormat.merge(configStr, builder);
-            } catch (ParseException e) {
-                fail("Failed to parse the perfetto text config file", e, device);
-            }
-            TraceConfig traceConfig = builder.build();
-
-            // Write the binary file and push it to the device.
-            File tempFile = null;
-            try (ByteArrayInputStream byteArray =
-                    new ByteArrayInputStream(traceConfig.toByteArray())) {
-                tempFile = FileUtil.createTempFile("trace", ".pb");
-                FileUtil.writeToFile(byteArray, tempFile);
-                if (!device.pushFile(tempFile, DEVICE_CONFIG_PATH)) {
-                    fail("Failed to push the perfetto file", null, device);
-                }
-            } catch (IOException e) {
-                fail("Failed to write the perfetto binary file", e, device);
-            } finally {
-                FileUtil.deleteFile(tempFile);
             }
         }
     }
