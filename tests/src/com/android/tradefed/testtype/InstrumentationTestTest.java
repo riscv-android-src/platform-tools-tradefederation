73c5f640e23c6dc05aa3c15c0dd212055f7bb327
Julien Desprez <jdesprez@google.com>
2019-12-13 09:27:06 -0800

    Collectors were missing forwarding for modules
    Collectors were introduced without modules in mind and
    missed the forwarding of modules level.
    
    Test: unit tests
    cts-tradefed run retry --retry 0
    Bug: 146180028
    
    Change-Id: I25dacd40e570e2e403742089a584cf449c0b55f2
    Merged-In: I25dacd40e570e2e403742089a584cf449c0b55f2

--- tests/src/com/android/tradefed/testtype/InstrumentationTestTest.java
+++ tests/src/com/android/tradefed/testtype/InstrumentationTestTest.java
@@ -977,8 +977,8 @@ public class InstrumentationTestTest {
         inOrder.verify(mInstrumentationTest).setRunnerArgs(runner.capture());
         inOrder.verify(mMockTestDevice, times(2))
                 .runInstrumentationTests(eq(runner.getValue()), any(ITestLifeCycleReceiver.class));
-
-        inOrder.verify(mMockListener).testRunStarted(TEST_PACKAGE_VALUE, 2);
+        inOrder.verify(mMockListener)
+                .testRunStarted(eq(TEST_PACKAGE_VALUE), eq(2), eq(0), anyLong());
         inOrder.verify(mMockListener).testStarted(eq(TEST1), anyLong());
         inOrder.verify(mMockListener).testEnded(eq(TEST1), anyLong(), eq(EMPTY_STRING_MAP));
         inOrder.verify(mMockListener).testStarted(eq(TEST2), anyLong());
