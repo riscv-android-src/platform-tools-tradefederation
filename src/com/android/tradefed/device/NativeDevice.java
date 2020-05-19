6eb9753067ceb655d7adbd11ad20306e38f84570
Julien Desprez <jdesprez@google.com>
2020-05-19 08:29:44 -0700

    Avoid NPE in isEncrypted
    Test: unit tests
    Bug: 157039160
    Change-Id: I2d4616620082ce802a0bd7ab8ad3d9ac959673d1

--- src/com/android/tradefed/device/NativeDevice.java
+++ src/com/android/tradefed/device/NativeDevice.java
@@ -3549,7 +3549,9 @@ public class NativeDevice implements IManagedTestDevice {
         if (output == null && isEncryptionSupported()) {
             CLog.w("Property ro.crypto.state is null on device %s", getSerialNumber());
         }
-
+        if (output == null) {
+            return false;
+        }
         return "encrypted".equals(output.trim());
     }
 
