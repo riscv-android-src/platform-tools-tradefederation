9fe5dcf38a3e623761e93db7828f5640df563af8
Julien Desprez <jdesprez@google.com>
2020-07-06 10:56:49 -0700

    Continue to create some error classification
    Test: unit tests
    Bug: 153374987
    Change-Id: Ie4ed03aaef80ebd14a16fa1e4db65bea440da8fb

--- test_result_interfaces/com/android/tradefed/result/error/DeviceErrorIdentifier.java
+++ test_result_interfaces/com/android/tradefed/result/error/DeviceErrorIdentifier.java
@@ -32,6 +32,7 @@ public enum DeviceErrorIdentifier implements ErrorIdentifier {
 
     FAILED_TO_LAUNCH_GCE(30_500, FailureStatus.LOST_SYSTEM_UNDER_TEST),
     FAILED_TO_CONNECT_TO_GCE(30_501, FailureStatus.LOST_SYSTEM_UNDER_TEST),
+    ERROR_AFTER_FLASHING(30_502, FailureStatus.LOST_SYSTEM_UNDER_TEST),
 
     DEVICE_UNAVAILABLE(30_750, FailureStatus.LOST_SYSTEM_UNDER_TEST);
 
