bb2548c45fd9517a57663da881d2f9065cd16ba8
Julien Desprez <jdesprez@google.com>
2020-10-19 10:35:35 -0700

    Update Target_preparer interfaces to use newest one
    Test: unit tests
    Bug: 171219657
    Change-Id: I6cf47345c944dde18a503b22ca169ed8816321f5

--- tests/src/com/android/tradefed/targetprep/AllTestAppsInstallSetupTest.java
+++ tests/src/com/android/tradefed/targetprep/AllTestAppsInstallSetupTest.java
@@ -4,6 +4,9 @@ import com.android.tradefed.build.IBuildInfo;
 import com.android.tradefed.build.IDeviceBuildInfo;
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.invoker.IInvocationContext;
+import com.android.tradefed.invoker.InvocationContext;
+import com.android.tradefed.invoker.TestInformation;
 import com.android.tradefed.util.FileUtil;
 
 import junit.framework.TestCase;
@@ -22,6 +25,8 @@ public class AllTestAppsInstallSetupTest extends TestCase {
     private IDeviceBuildInfo mMockBuildInfo;
     private ITestDevice mMockTestDevice;
 
+    private TestInformation mTestInfo;
+
     /**
      * {@inheritDoc}
      */
@@ -34,13 +39,21 @@ public class AllTestAppsInstallSetupTest extends TestCase {
         EasyMock.expect(mMockTestDevice.getSerialNumber()).andStubReturn(SERIAL);
         EasyMock.expect(mMockTestDevice.getDeviceDescriptor()).andStubReturn(null);
         EasyMock.expect(mMockTestDevice.isAppEnumerationSupported()).andStubReturn(false);
+        IInvocationContext context = new InvocationContext();
+        context.addAllocatedDevice("device", mMockTestDevice);
+        context.addDeviceBuildInfo("device", mMockBuildInfo);
+        mTestInfo = TestInformation.newBuilder().setInvocationContext(context).build();
     }
 
-    public void testNotIDeviceBuildInfo() throws DeviceNotAvailableException {
+    public void testNotIDeviceBuildInfo() throws Exception {
         IBuildInfo mockBuildInfo = EasyMock.createMock(IBuildInfo.class);
+        IInvocationContext context = new InvocationContext();
+        context.addAllocatedDevice("device", mMockTestDevice);
+        context.addDeviceBuildInfo("device", mockBuildInfo);
+        mTestInfo = TestInformation.newBuilder().setInvocationContext(context).build();
         EasyMock.replay(mockBuildInfo, mMockTestDevice);
         try {
-            mPrep.setUp(mMockTestDevice, mockBuildInfo);
+            mPrep.setUp(mTestInfo);
             fail("Should have thrown a TargetSetupError");
         } catch (TargetSetupError e) {
             // expected
@@ -49,11 +62,11 @@ public class AllTestAppsInstallSetupTest extends TestCase {
         EasyMock.verify(mockBuildInfo, mMockTestDevice);
     }
 
-    public void testNoTestDir() throws DeviceNotAvailableException {
+    public void testNoTestDir() throws Exception {
         EasyMock.expect(mMockBuildInfo.getTestsDir()).andStubReturn(new File(""));
         EasyMock.replay(mMockBuildInfo, mMockTestDevice);
         try {
-            mPrep.setUp(mMockTestDevice, mMockBuildInfo);
+            mPrep.setUp(mTestInfo);
             fail("Should have thrown a TargetSetupError");
         } catch (TargetSetupError e) {
             assertEquals("Failed to find a valid test zip directory.", e.getMessage());
@@ -86,7 +99,7 @@ public class AllTestAppsInstallSetupTest extends TestCase {
             EasyMock.expect(mMockTestDevice.installPackage((File)EasyMock.anyObject(),
                     EasyMock.eq(true))).andReturn(null).times(3);
             EasyMock.replay(mMockBuildInfo, mMockTestDevice);
-            mPrep.setUp(mMockTestDevice, mMockBuildInfo);
+            mPrep.setUp(mTestInfo);
             EasyMock.verify(mMockBuildInfo, mMockTestDevice);
         } finally {
             FileUtil.recursiveDelete(testDir);
@@ -102,7 +115,7 @@ public class AllTestAppsInstallSetupTest extends TestCase {
             EasyMock.expect(mMockTestDevice.installPackage((File)EasyMock.anyObject(),
                     EasyMock.eq(true), EasyMock.eq("--force-queryable"))).andReturn(null);
             EasyMock.replay(mMockBuildInfo, mMockTestDevice);
-            mPrep.setUp(mMockTestDevice, mMockBuildInfo);
+            mPrep.setUp(mTestInfo);
             EasyMock.verify(mMockBuildInfo, mMockTestDevice);
         } finally {
             FileUtil.recursiveDelete(testDir);
