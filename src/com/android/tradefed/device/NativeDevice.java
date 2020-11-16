7619cee7e35e76204a1b614a0cc3eb570eb88107
Julien Desprez <jdesprez@google.com>
2020-11-16 10:11:03 -0800

    Reset content provider in case of powerwash
    This will allow the utility to be re-installed
    
    Test: unit tests
    Bug: None
    Change-Id: Iecc4df90138a53db024dcf573caf124fc25adbd9

--- src/com/android/tradefed/device/NativeDevice.java
+++ src/com/android/tradefed/device/NativeDevice.java
@@ -5052,7 +5052,7 @@ public class NativeDevice implements IManagedTestDevice {
     }
 
     /** Reset the flag for content provider setup in order to trigger it again. */
-    void resetContentProviderSetup() {
+    protected void resetContentProviderSetup() {
         mShouldSkipContentProviderSetup = false;
     }
 
