18d699a1a25532c153fde1cfdb4842ae93b08802
Julien Desprez <jdesprez@google.com>
2021-01-22 11:51:01 -0800

    Fix some of HostTest exception and reporting of errors
    Test: unit tests
    Bug: 178213998
    Change-Id: I0ac0ebfaf361ebdaddeaa9d792a2039ae9d54b02

--- src/com/android/tradefed/result/EventsLoggerListener.java
+++ src/com/android/tradefed/result/EventsLoggerListener.java
@@ -86,12 +86,12 @@ public class EventsLoggerListener implements ITestInvocationListener {
 
     @Override
     public void testRunFailed(FailureDescription failure) {
-        writeToFile(String.format("    [run failed with %s]", failure));
+        writeToFile(String.format("        [run failed with %s]\n", failure));
     }
 
     @Override
     public void testRunFailed(String errorMessage) {
-        writeToFile(String.format("    [run failed with %s]", errorMessage));
+        writeToFile(String.format("        [run failed with %s]\n", errorMessage));
     }
 
     @Override
