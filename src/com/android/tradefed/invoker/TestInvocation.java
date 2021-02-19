4bc7743cbde9a5b5c59d9eae2c6470de5930b912
Julien Desprez <jdesprez@google.com>
2021-02-19 11:44:56 -0800

    Fix a bit the logging of adb interaction
    - didn't quite work before.
    
    Test: unit tests,
    run host --class com.android.tradefed.build.BuildInfoTest --class com.android.tradefed.build.DeviceBuildInfoTest -n --shard-count 2
    run host --class com.android.tradefed.build.BuildInfoTest --class
    com.android.tradefed.build.DeviceBuildInfoTest -n --shard-count 2
    --use-sandbox
    Bug: 180721780
    
    Change-Id: I46b0928202003cc79cbba0c3f36758135622bb32

--- src/com/android/tradefed/invoker/TestInvocation.java
+++ src/com/android/tradefed/invoker/TestInvocation.java
@@ -1321,7 +1321,7 @@ public class TestInvocation implements ITestInvocation {
 
     /** Measure the size of the work folder. */
     private Long measureWorkFolderSize(IConfiguration config, File workFolder) {
-        if (workFolder == null) {
+        if (workFolder == null || !workFolder.exists()) {
             return null;
         }
         // Only measure in parent process
@@ -1329,6 +1329,14 @@ public class TestInvocation implements ITestInvocation {
                 .getInvocationData()
                 .containsKey(SubprocessTfLauncher.SUBPROCESS_TAG_NAME)) {
             return null;
+        }
+
+        Object obj = config.getConfigurationObject(ShardHelper.LAST_SHARD_DETECTOR);
+        if (obj != null) {
+            LastShardDetector lastShardDetector = (LastShardDetector) obj;
+            if (!lastShardDetector.isLastShardDone()) {
+                return null;
+            }
         }
         Path folder = workFolder.toPath();
         try {
