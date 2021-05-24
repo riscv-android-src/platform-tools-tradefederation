866f1b33dc32455f545fd8cd7e710e6b83dc40ee
Julien Desprez <jdesprez@google.com>
2021-05-24 14:33:50 -0700

    Report pair metrics together to handle repeat
    Test: unit tests
    Bug: 179696292
    Change-Id: I41344683c79adc565e83e32ea775db96d408bb58

--- src/com/android/tradefed/invoker/TestInvocation.java
+++ src/com/android/tradefed/invoker/TestInvocation.java
@@ -961,6 +961,8 @@ public class TestInvocation implements ITestInvocation {
                     invokeFetchBuild(info, config, rescheduler, listener, invocationPath);
             long end = System.currentTimeMillis();
             InvocationMetricLogger.addInvocationMetrics(InvocationMetricKey.FETCH_BUILD_END, end);
+            InvocationMetricLogger.addInvocationPairMetrics(
+                    InvocationMetricKey.FETCH_BUILD_PAIR, start, end);
             long fetchBuildDuration = end - start;
             InvocationMetricLogger.addInvocationMetrics(
                     InvocationMetricKey.FETCH_BUILD, fetchBuildDuration);
