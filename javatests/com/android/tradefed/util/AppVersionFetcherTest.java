801d358647bdf8aa4c3916b48c3d4a812001c9b9
frankfeng <frankfeng@google.com>
2021-07-01 16:59:23 -0700

    Convet to Mockito: AppVersionFetcherTest
    Test: unit test
    Bug: 186588563
    Change-Id: I8570d346b583a8e4f75efa45dd5e3e873deb0ed9

--- javatests/com/android/tradefed/util/AppVersionFetcherTest.java
+++ javatests/com/android/tradefed/util/AppVersionFetcherTest.java
@@ -16,16 +16,19 @@
 
 package com.android.tradefed.util;
 
+import static org.mockito.Mockito.when;
+
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.util.AppVersionFetcher.AppVersionInfo;
 
-import org.easymock.EasyMock;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /** Unit tests for {@link AppVersionFetcher} */
 @RunWith(JUnit4.class)
@@ -35,86 +38,74 @@ public class AppVersionFetcherTest {
     private static final String VERSION_CODE_CMD = "dumpsys package mypackage | grep versionCode=";
     private static final String VERSION_NAME_CMD = "dumpsys package mypackage | grep versionName=";
 
-    private ITestDevice mMockDevice;
+    @Mock ITestDevice mMockDevice;
 
     @Before
-    public void setup() {
-        mMockDevice = EasyMock.createMock(ITestDevice.class);
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testFetchVersionCode_valid() throws DeviceNotAvailableException {
-        EasyMock.expect(mMockDevice.executeShellCommand(VERSION_CODE_CMD))
-                .andReturn("   versionCode=780616927 minSdk=25 targetSdk=25");
-        EasyMock.replay(mMockDevice);
+        when(mMockDevice.executeShellCommand(VERSION_CODE_CMD))
+                .thenReturn("   versionCode=780616927 minSdk=25 targetSdk=25");
+
         Assert.assertEquals(
                 "780616927",
                 AppVersionFetcher.fetch(mMockDevice, PACKAGE_NAME, AppVersionInfo.VERSION_CODE));
-        EasyMock.verify(mMockDevice);
     }
 
     @Test
     public void testFetchVersionName_valid() throws DeviceNotAvailableException {
-        EasyMock.expect(mMockDevice.executeShellCommand(VERSION_NAME_CMD))
-                .andReturn("    versionName=2.7.0.179109344");
-        EasyMock.replay(mMockDevice);
+        when(mMockDevice.executeShellCommand(VERSION_NAME_CMD))
+                .thenReturn("    versionName=2.7.0.179109344");
+
         Assert.assertEquals(
                 "2.7.0.179109344",
                 AppVersionFetcher.fetch(mMockDevice, PACKAGE_NAME, AppVersionInfo.VERSION_NAME));
-        EasyMock.verify(mMockDevice);
     }
 
     @Test
     public void testFetchVersionCode_validMultipleLines() throws DeviceNotAvailableException {
-        EasyMock.expect(mMockDevice.executeShellCommand(VERSION_CODE_CMD))
-                .andReturn(
+        when(mMockDevice.executeShellCommand(VERSION_CODE_CMD))
+                .thenReturn(
                         "   versionCode=456 minSdk=25 targetSdk=25"
                                 + "\nversionCode=123 minSdk=25 targetSdk=25");
-        EasyMock.replay(mMockDevice);
+
         Assert.assertEquals(
                 "456",
                 AppVersionFetcher.fetch(mMockDevice, PACKAGE_NAME, AppVersionInfo.VERSION_CODE));
-        EasyMock.verify(mMockDevice);
     }
 
     @Test
     public void testFetchVersionName_validMultipleLines() throws DeviceNotAvailableException {
-        EasyMock.expect(mMockDevice.executeShellCommand(VERSION_NAME_CMD))
-                .andReturn("    versionName=2.7.0.1\n    versionName=2.7.0.0");
-        EasyMock.replay(mMockDevice);
+        when(mMockDevice.executeShellCommand(VERSION_NAME_CMD))
+                .thenReturn("    versionName=2.7.0.1\n    versionName=2.7.0.0");
+
         Assert.assertEquals(
                 "2.7.0.1",
                 AppVersionFetcher.fetch(mMockDevice, PACKAGE_NAME, AppVersionInfo.VERSION_NAME));
-        EasyMock.verify(mMockDevice);
     }
 
     @Test
     public void testFetchVersionCode_invalidResponse() throws DeviceNotAvailableException {
-        EasyMock.expect(mMockDevice.executeShellCommand(VERSION_CODE_CMD))
-                .andReturn("invalid response");
-        EasyMock.replay(mMockDevice);
+        when(mMockDevice.executeShellCommand(VERSION_CODE_CMD)).thenReturn("invalid response");
 
         try {
             AppVersionFetcher.fetch(mMockDevice, PACKAGE_NAME, AppVersionInfo.VERSION_CODE);
             Assert.fail();
         } catch (IllegalStateException expectedException) {
         }
-
-        EasyMock.verify(mMockDevice);
     }
 
     @Test
     public void testFetchVersionName_invalidResponse() throws DeviceNotAvailableException {
-        EasyMock.expect(mMockDevice.executeShellCommand(VERSION_NAME_CMD))
-                .andReturn("invalid response");
-        EasyMock.replay(mMockDevice);
+        when(mMockDevice.executeShellCommand(VERSION_NAME_CMD)).thenReturn("invalid response");
 
         try {
             AppVersionFetcher.fetch(mMockDevice, PACKAGE_NAME, AppVersionInfo.VERSION_NAME);
             Assert.fail();
         } catch (IllegalStateException expectedException) {
         }
-
-        EasyMock.verify(mMockDevice);
     }
 }
