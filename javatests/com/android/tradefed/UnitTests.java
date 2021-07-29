9f5238bf4a68fe569196ce993d8c89e88b3c2bd5
Jonathan Scott <scottjonathan@google.com>
2021-07-29 22:19:37 +0100

    Add PackageInstalledModuleController.
    This can be used to skip a module if a package which it is dependent on
    is not available.
    
    Test: tapas tradefed-all && m && tools/tradefederation/core/tradefed.sh run singleCommand host -n --class com.android.tradefed.testtype.suite.module.PackageInstalledModuleControllerTest
    Bug: 194890455
    Change-Id: I02a7c96bc4644a765376daeadb4afc39f63abc1a

--- javatests/com/android/tradefed/UnitTests.java
+++ javatests/com/android/tradefed/UnitTests.java
@@ -343,6 +343,7 @@ import com.android.tradefed.testtype.suite.module.CarModuleControllerTest;
 import com.android.tradefed.testtype.suite.module.DeviceFeatureModuleControllerTest;
 import com.android.tradefed.testtype.suite.module.MinApiLevelModuleControllerTest;
 import com.android.tradefed.testtype.suite.module.NativeBridgeModuleControllerTest;
+import com.android.tradefed.testtype.suite.module.PackageInstalledModuleControllerTest;
 import com.android.tradefed.testtype.suite.module.ShippingApiLevelModuleControllerTest;
 import com.android.tradefed.testtype.suite.params.InstantAppHandlerTest;
 import com.android.tradefed.testtype.suite.params.MainlineModuleHandlerTest;
@@ -869,6 +870,7 @@ import org.junit.runners.Suite.SuiteClasses;
     DeviceFeatureModuleControllerTest.class,
     MinApiLevelModuleControllerTest.class,
     NativeBridgeModuleControllerTest.class,
+    PackageInstalledModuleControllerTest.class,
     ShippingApiLevelModuleControllerTest.class,
 
     // testtype/suite/params
